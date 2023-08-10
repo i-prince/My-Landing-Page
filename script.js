49ce35188c8e48ffd629d896a49a89825ba4020c
i-prince <molatonny@gmail.com>
2023-08-11 00:18:41 +0300

    Add netflify link

--- script.js
+++ script.js
@@ -1,97 +1,89 @@
 // Fetching the player stats data on page load
 // Top scorer data
-const baseUrl = 'https://github.com/i-prince/My-Landing-Page/blob/main/db.json'; // Replace with your actual backend URL
+const baseUrl = 'https://spectacular-lebkuchen-7b3879.netlify.app'; // Remove trailing slash from URL
 
-
-fetch('${baseUrl}/top_scorer')
+fetch(`${baseUrl}/top_scorer`) // Use the backticks (``) for string interpolation
 .then((topScorerResource) => topScorerResource.json())
 .then((topScorerData) => {
     const topScorerImage = document.getElementById('topScorerImage');
     const topScorerText = document.getElementById('topScorerText');
 
-    topScorerImage.src = topScorerData.image; // Rendering the image to the webpage
-    topScorerText.textContent = `${topScorerData.name} has scored ${topScorerData.goals} goals and provided ${topScorerData.assists} assists.`; // rendering the text content to the webpage 
+    topScorerImage.src = topScorerData.image;
+    topScorerText.textContent = `${topScorerData.name} has scored ${topScorerData.goals} goals and provided ${topScorerData.assists} assists.`;
 })
-.catch((topScorerError) => alert('invalid request'))
+.catch((topScorerError) => alert('Invalid request'));
 
 // Top assister data
-fetch('http://localhost:3000/top_assister')
+fetch(`${baseUrl}/top_assister`)
 .then((topAssisterResource) => topAssisterResource.json())
 .then((topAssisterData) => {
     const topAssisterImage = document.getElementById('topAssisterImage');
     const topAssisterText = document.getElementById('topAssisterText');
 
     topAssisterImage.src = topAssisterData.image;
-    topAssisterText.textContent = `${topAssisterData.name} has scored ${topAssisterData.goals} and provided ${topAssisterData.assists} assists`
+    topAssisterText.textContent = `${topAssisterData.name} has scored ${topAssisterData.goals} and provided ${topAssisterData.assists} assists`;
 })
-.catch((topAssisterError) => alert('invalid request'))
+.catch((topAssisterError) => alert('Invalid request'));
 
 // Top goalkeeper data
-fetch('http://localhost:3000/top_goalkeeper')
+fetch(`${baseUrl}/top_goalkeeper`)
 .then((topGoalkeeperResource) => topGoalkeeperResource.json())
 .then((topGoalkeeperData) => {
     const topGoalkeeperImage = document.getElementById('topGoalkeeperImage');
     const topGoalkeeperText = document.getElementById('topGoalkeeperText');
 
     topGoalkeeperImage.src = topGoalkeeperData.image;
-    topGoalkeeperText.textContent = `${topGoalkeeperData.name} has produced ${topGoalkeeperData.clean_sheets} clean sheets and conceded ${topGoalkeeperData.goals_conceded} goals only`
+    topGoalkeeperText.textContent = `${topGoalkeeperData.name} has produced ${topGoalkeeperData.clean_sheets} clean sheets and conceded ${topGoalkeeperData.goals_conceded} goals only`;
 })
-.catch((topGoalkeeperError) => alert('invalid request'))
+.catch((topGoalkeeperError) => alert('Invalid request'));
 
 // Promising talent data
-fetch('http://localhost:3000/promising_talent')
+fetch(`${baseUrl}/promising_talent`)
 .then((promisingTalentResource) => promisingTalentResource.json())
 .then((promisingTalentData) => {
     const promisingTalentImage = document.getElementById('promisingTalentImage');
     const promisingTalentText = document.getElementById('promisingTalentText');
 
     promisingTalentImage.src = promisingTalentData.image;
-    promisingTalentText.textContent = `${promisingTalentData.name} is a ${promisingTalentData.position} and is ${promisingTalentData.age} years old`
+    promisingTalentText.textContent = `${promisingTalentData.name} is a ${promisingTalentData.position} and is ${promisingTalentData.age} years old`;
 })
-.catch((promisingTalentError) => alert('invalid request'))
+.catch((promisingTalentError) => alert('Invalid request'));
 
 // Signing up for an account
-    document.getElementById("myForm").addEventListener("submit", function(event) {
-        event.preventDefault();
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault();
 
-        const email = document.getElementById("email").value;
-        const name = document.getElementById("name").value;
-        const password = document.getElementById("password").value;
+    const email = document.getElementById("email").value;
+    const name = document.getElementById("name").value;
+    const password = document.getElementById("password").value;
 
-        const userData = {
-            email: email,
-            name: name,
-            password: password
-        };
+    const userData = {
+        email: email,
+        name: name,
+        password: password
+    };
 
-        // Sending the retrieved data to my JSON using POST
-        const signUpForm = document.getElementById("signUpForm");
+    // Sending the retrieved data to your JSON using POST
+    const signUpForm = document.getElementById("signUpForm");
 
-        fetch('http://localhost:3000/new_user', {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(userData)
-        })
-        .then((newUserResource) => newUserResource.json())
-        .then((newUserData) => {
-            // Display success alert
-            document.getElementById("successAlert").style.display = "block";
+    fetch(`${baseUrl}/new_user`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userData)
+    })
+    .then((response) => response.json())
+    .then((newUserData) => {
+        // Display success alert
+        document.getElementById("successAlert").style.display = "block";
 
-            // Hide the alert after a delay (e.g., 5 seconds)
-            setTimeout(() => {
-                document.getElementById("successAlert").style.display = "none";
-            }, 5000); // Adjust the time delay as needed
+        // Hide the alert after a delay (e.g., 5 seconds)
+        setTimeout(() => {
+            document.getElementById("successAlert").style.display = "none";
+        }, 5000);
 
-            signUpForm.reset();
-        })
-
-        .catch((newUserError) => console.log("Invalid Request"));
-    });
-
-
-
-
-
-
+        signUpForm.reset();
+    })
+    .catch((newUserError) => console.log("Invalid Request"));
+});
