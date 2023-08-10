5b6d05ba4e0e40a0d8769ff19647f1e4a4b3f02c
i-prince <molatonny@gmail.com>
2023-08-11 01:07:40 +0300

    fix netlify hosting error

--- functions/fetch-promising-talent.js
+++ functions/fetch-promising-talent.js
@@ -1,9 +1,19 @@
+const fetch = require('node-fetch');
+
 exports.handler = async (event, context) => {
-    const data = require('./https://spectacular-lebkuchen-7b3879.netlify.app/db.json');
-    const promisingTalent = data.promising_talent;
-  
-    return {
-      statusCode: 200,
-      body: JSON.stringify(promisingTalent)
-    };
-  };
+    try {
+        const response = await fetch('https://spectacular-lebkuchen-7b3879.netlify.app/db.json');
+        const data = await response.json();
+        const promisingTalent = data.promising_talent;
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(promisingTalent)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal Server Error' })
+        };
+    }
+};
--- functions/fetch-top-scorer.js
+++ functions/fetch-top-scorer.js
@@ -1,10 +1,17 @@
 exports.handler = async (event, context) => {
-    const data = require('./https://spectacular-lebkuchen-7b3879.netlify.app/db.json');
-    const topScorer = data.top_scorer;
-  
-    return {
-      statusCode: 200,
-      body: JSON.stringify(topScorer)
-    };
-  };
-  
+  try {
+      const response = await fetch('https://spectacular-lebkuchen-7b3879.netlify.app/db.json');
+      const data = await response.json();
+      const topScorer = data.top_scorer;
+
+      return {
+          statusCode: 200,
+          body: JSON.stringify(topScorer)
+      };
+  } catch (error) {
+      return {
+          statusCode: 500,
+          body: JSON.stringify({ error: 'Internal Server Error' })
+      };
+  }
+};
